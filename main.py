3a69f4261ccb0c67954264fd3c745c1b992cbe1d
Ozan Halis Ilter <ozanhalisilter@gmail.com>
2023-06-06 12:05:16 +0300

    Update main.py

--- main.py
+++ main.py
@@ -4,17 +4,23 @@ import base64
 
 app = FastAPI()
 
+def generate_otp(user_id: str) -> str:
+    user_id_base32 = base64.b32encode(user_id.encode()).decode()
+    totp = pyotp.TOTP(user_id_base32)
+    return f"{user_id_base32}_{totp.now()}"
+
+def validate_otp(string: str) -> bool:
+    user_id_base32, otp = string.split("_")
+    user_id = base64.b32decode(user_id_base32).decode()
+    totp = pyotp.TOTP(user_id_base32)
+    return string == f"{user_id_base32}_{otp}" and totp.verify(otp)
+
 @app.get('/otp/{user_id}')
-def Generate_OTP(user_id: str):
-    totp = pyotp.TOTP(user_id)
-    return {"qr_string" : user_id+"_"+totp.now()}
+def generate_otp_endpoint(user_id: str):
+    otp = generate_otp(user_id)
+    return {"qr_string": otp}
 
 @app.get('/validate/{string}')
-def Validate_OTP(string:str):
-    user_id = string.split("_")[0]
-    otp = string.split("_")[1]
-
-    totp = pyotp.TOTP(user_id)
-    is_true = user_id+"_"+totp.now() == user_id+"_"+otp
-    return {"validation" : is_true}
-
+def validate_otp_endpoint(string: str):
+    is_valid = validate_otp(string)
+    return {"validation": is_valid}
