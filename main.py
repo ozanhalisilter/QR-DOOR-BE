d703559e3e1db711895189356807883739faddb7
Ozan Halis Ilter <ozanhalisilter@gmail.com>
2023-06-06 11:21:11 +0300

    add otp

--- main.py
+++ main.py
@@ -1,13 +1,19 @@
 from fastapi import FastAPI
+import pyotp
 
 app = FastAPI()
 
+@app.get('/otp/{user_id}')
+def Generate_OTP(user_id: str):
+    totp = pyotp.TOTP(user_id)
+    return user_id+"_"+totp.now()
 
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
 
+@app.get('/validate/{string}')
+def Validate_OTP(string:str):
+    user_id = string.split("_")[0]
+    otp = string.split("_")[1]
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int):
-    return {"item_id": item_id}
+    totp = pyotp.TOTP(user_id)
+
+    return user_id+"_"+totp.now() == user_id+"_"+otp
