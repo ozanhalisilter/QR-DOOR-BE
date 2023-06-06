e15e7ad68bcd5b55ca11f8904e5b07454e85be9d
Ozan Halis Ilter <ozanhalisilter@gmail.com>
2023-06-06 11:50:28 +0300

    add base32

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 from fastapi import FastAPI
 import pyotp
+import base64
 
 app = FastAPI()
 
 @app.get('/otp/{user_id}')
 def Generate_OTP(user_id: str):
-    totp = pyotp.TOTP(user_id)
+    encoded_user_id = base64.b32encode(user_id)
+    totp = pyotp.TOTP(encoded_user_id)
     return user_id+"_"+totp.now()
 
 @app.get('/{string}}')
@@ -14,7 +16,7 @@ def test(string: str):
 
 @app.get('/validate/{string}')
 def Validate_OTP(string:str):
-    user_id = string.split("_")[0]
+    user_id = base64.b32decode(string.split("_")[0])
     otp = string.split("_")[1]
 
     totp = pyotp.TOTP(user_id)
