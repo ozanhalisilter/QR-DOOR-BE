e21ef658019230b0f9c549536cb0cfb56ed1b1ad
Ozan Halis Ilter <ozanhalisilter@gmail.com>
2023-06-06 11:56:33 +0300

    Update main.py

--- main.py
+++ main.py
@@ -6,19 +6,15 @@ app = FastAPI()
 
 @app.get('/otp/{user_id}')
 def Generate_OTP(user_id: str):
-    encoded_user_id = base64.b32encode(user_id)
-    totp = pyotp.TOTP(encoded_user_id)
-    return user_id+"_"+totp.now()
-
-@app.get('/{string}}')
-def test(string: str):
-    return string
+    totp = pyotp.TOTP(user_id)
+    return {"qr_string" : user_id+"_"+totp.now()}
 
 @app.get('/validate/{string}')
 def Validate_OTP(string:str):
-    user_id = base64.b32decode(string.split("_")[0])
+    user_id = string.split("_")[0]
     otp = string.split("_")[1]
 
     totp = pyotp.TOTP(user_id)
+    is_true = user_id+"_"+totp.now() == user_id+"_"+otp
+    return {"validation" : is_true}
 
-    return user_id+"_"+totp.now() == user_id+"_"+otp
